e575db3d05f98c12b376332ad810e3d75a92cc2a
Giovanni Naccarato <naccarato2005@gmail.com>
2019-03-18 12:30:33 -0400

    Updates

---
+++ precache-manifest.593236943f9b1a7183a1dfc5a50d4b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52c5bd88a3bf0778a6ef",
+    "url": "/recipe-search/static/css/main.6b403d6b.chunk.css"
+  },
+  {
+    "revision": "52c5bd88a3bf0778a6ef",
+    "url": "/recipe-search/static/js/main.8d937256.chunk.js"
+  },
+  {
+    "revision": "ec45451ef51fdad6195b",
+    "url": "/recipe-search/static/js/runtime~main.e553c534.js"
+  },
+  {
+    "revision": "ffd0f0e0bcdcf6947a9a",
+    "url": "/recipe-search/static/css/2.85182b8d.chunk.css"
+  },
+  {
+    "revision": "ffd0f0e0bcdcf6947a9a",
+    "url": "/recipe-search/static/js/2.e45dedbb.chunk.js"
+  },
+  {
+    "revision": "db2c79bf2a26c97f6d9d1e4ee93f546e",
+    "url": "/recipe-search/index.html"
+  }
+];
